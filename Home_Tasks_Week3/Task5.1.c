10dfda185258c613618e53538f6275af3dd46ab8
TheRealGamertube <75208972+TheRealGamertube@users.noreply.github.com>
2026-09-05 23:18:05 +0500

    Delete Home_Tasks_Week3 directory

--- Home_Tasks_Week3/Task5.1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(void)
-{
-	char ch;
-	printf("Enter a character: ");
-	ch = getchar();
-	putchar(ch);
-}
--- Home_Tasks_Week3/Task5.2.c
+++
@@ -1,9 +0,0 @@
-#include <Stdio.h>
-void main(void)
-{
-	char name[50];
-	printf("Enter your name: ");
-	fgets(name, 50, stdin);
-	printf("Your name is ");
-	puts(name);
-}
